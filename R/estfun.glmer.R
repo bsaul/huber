380270cf1396b546b2c85e39b8a9f6acab21cd87
Bradley Saul <bradleysaul@gmail.com>
2016-04-07 13:34:17 -0400

    disables objFun.glmerMod.generalized -> needs some work. Compacts estfun.glmerMod arguments grad.method and grad.method.args to grad_options. grad_options takes a list(), and passes this to numDeriv::grad() via do.call().

--- R/estfun.glmer.R
+++ R/estfun.glmer.R
@@ -8,35 +8,50 @@
 #' @export
 #------------------------------------------------------------------------------#
 
-estfun.glmerMod <- function(x, grad.method, grad.method.args = list(), ...)
+estfun.glmerMod <- function(x, grad_options = NULL, ...)
 {
   xmat   <- lme4::getME(x, 'X')
   resp   <- lme4::getME(x, 'y')
   parms  <- unlist(lme4::getME(x, c('beta', 'theta')))
   clust  <- lme4::getME(x, 'flist')
   family <- x@resp$family$family
-  # objective.fun <- objFun.glmerMod(family)
-  link_obj <- stats::make.link(stats::family(x)$link)
-  gen_objFun_args <- list(
-    parms = parms,
-    response = resp,
-    xmatrix = xmat,
-    inv_link_fun = link_obj$linkinv
-  )
-  objective.fun <- do.call(objFun.glmerMod.generalized, args = gen_objFun_args)
+  objective.fun <- objFun.glmerMod(family)
 
+  #  link_obj <- stats::make.link(stats::family(x)$link)
+#
+#   gen_objFun_args <- list(
+#     parms = parms,
+#     response = resp,
+#     xmatrix = xmat,
+#     inv_link_fun = link_obj$linkinv
+#   )
+
+  ## Warnings ##
   if(length(lme4::getME(x, 'theta')) > 1){
-    stop('Not yet sure how to handle > 1 random effect')
+    stop('estfun.glmer currently does not handle >1  random effect')
   }
+#   objective.fun <- objFun.glmerMod.generalized(
+#                         parms = parms,
+#                         response = resp,
+#                         xmatrix = xmat,
+#                         inv_link_fun = link_obj$linkinv)
+
 
   out <- by(cbind(resp, xmat), clust, simplify = F, FUN = function(x) {
     x <- as.matrix(x)
-    numDeriv::grad(objective.fun, x = parms,
-                   response = x[ , 1], xmatrix = x[ , -1],
-                   method = grad.method, method.args = grad.method.args)
+
+    grad_args <- append(list(objective.fun,
+                             x = parms,
+                             response = x[ , 1],
+                             xmatrix = x[ , -1]) ,
+                        grad_options)
+
+    return( do.call(numDeriv::grad, args = grad_args) )
+#     numDeriv::grad(objective.fun,
+#                    method = grad.method, method.args = grad.method.args)
   })
 
-  do.call('rbind', out)
+  return( do.call('rbind', out) )
 }
 
 #------------------------------------------------------------------------------#
@@ -53,21 +68,21 @@ objFun.glmerMod <- function(family, ...){
          stop('Objective function not defined'))
 }
 
-#------------------------------------------------------------------------------#
+#' ------------------------------------------------------------------------------#
 #' glmer Objective Function for Logistic-Normal Likelihood
 #'
-#'@param vector of parameters
-#'@param response
-#'@param xmatrix
-#'@export
-#------------------------------------------------------------------------------#
+#' @param vector of parameters
+#' @param response
+#' @param xmatrix
+#' @export
+#' ------------------------------------------------------------------------------#
 
 objFun.glmerMod.binomial <- function(parms, response, xmatrix)
 {
   # Logistic-Normal Model
   integrand <- function(b, response, xmatrix, parms){
-    lc <- outer(xmatrix %*% parms[-length(parms)], b, '+')
-    h  <- apply(lc, 3, function(x) dbinom(response, 1, plogis(x) ) )
+    lp <- outer(xmatrix %*% parms[-length(parms)], b, '+')
+    h  <- apply(lp, 3, function(x) dbinom(response, 1, plogis(x) ) )
     hh <- apply(h, 2, prod)
     hh * dnorm(b, mean = 0, sd = parms[length(parms)])
   }
@@ -86,14 +101,14 @@ objFun.glmerMod.binomial <- function(parms, response, xmatrix)
 #'@param xmatrix
 #'@param inv_link_fun
 #'@export
-#'#------------------------------------------------------------------------------#
-#'
+#------------------------------------------------------------------------------#
+
 objFun.glmerMod.generalized <- function(parms, response, xmatrix, inv_link_fun)
 {
   # Logistic-Normal Model
   integrand <- function(b, response, xmatrix, parms){
-    lc <- outer(xmatrix %*% parms[-length(parms)], b, '+')
-    h  <- apply(lc, 3, function(x) dbinom(response, 1, plogis(x) ) )
+    lp <- outer(xmatrix %*% parms[-length(parms)], b, '+')
+    h  <- apply(lp, 3, function(x) dbinom(response, 1, plogis(x) ) )
     hh <- apply(h, 2, prod)
     hh * dnorm(b, mean = 0, sd = parms[length(parms)])
   }
@@ -102,5 +117,3 @@ objFun.glmerMod.generalized <- function(parms, response, xmatrix, inv_link_fun)
                 response = response, xmatrix = xmatrix)$value)
 
 }
-
-
