161b5c610ddb4837afdb168451c661d30b880413
Bradley Saul <bradley@BigScreen.local>
2016-04-13 18:46:00 -0400

    initial commit to create models from list of arguments

---
+++ R/make_models.R
@@ -0,0 +1,23 @@
+#------------------------------------------------------------------------------#
+#' Makes model objects from list of models
+#'
+#' @param models_args list of model arguments
+#' @param data used for models
+#' @export
+#------------------------------------------------------------------------------#
+
+make_models <- function(model_args, data)
+{
+  out <- lapply(model_args, function(x){
+    method <- match.fun(x$method)
+    if(is.null(x$user)) x$user <- FALSE
+
+    if(x$user == TRUE){
+      NULL
+    } else {
+      args <- append(x$options, list(formula = x$formula, data = data) )
+      do.call(method, args = args)
+    }
+  })
+  return(out)
+}
