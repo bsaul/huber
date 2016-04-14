c8eb6a2dbcb579d7f6a68fbffd04643f08433733
Bradley Saul <bradleysaul@gmail.com>
2016-04-14 11:07:56 -0400

    bread_glmer takes psi.glmerMod and takes mean of the derivative of the psi function

--- R/bread_glmer.R
+++ R/bread_glmer.R
@@ -1,16 +1,24 @@
 #------------------------------------------------------------------------------#
 #' glmer bread matrices
 #'
-#' Currently supports Logistic-Normal model with univariate random effect.
-#' @param grad.method method passed to \code{\link[numDeriv]{grad}}
-#' @param grad.method.args method.args passed to \code{\link[numDeriv]{grad}}
-#' @importFrom sandwich estfun
+#' Currently supports Logistic-Normal model with single random effect.
+#' @param x `glmer` object
+#' @param grad_method method passed to \code{\link[numDeriv]{hessian}}
+#' @param grad_options method.args passed to \code{\link[numDeriv]{hessian}}
+#' @importFrom sandwich bread
 #' @export
 #------------------------------------------------------------------------------#
 
-bread.glmerMod <- function(x, grad_options = NULL, ...)
+bread.glmerMod <- function(x, grad_method, grad_options = NULL, ...)
 {
-  psi.glmerMod(x = x, grad_options = grad_options, hessian = TRUE, ...)
+  psi_prime <- psi.glmerMod(x = x, grad_method,
+                           grad_options = grad_options,
+                           deriv = TRUE, ...)
+  psi_prime <- array(unlist(psi_prime),
+                     dim = c(nrow(psi_prime[[1]]),
+                             ncol(psi_prime[[1]]),
+                             length(psi_prime) ) )
+  return( -apply(psi_prime, 1:2, mean) )
 }
 
 
